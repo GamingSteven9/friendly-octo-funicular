72e02824f90b9102470b257975867d3549bee414
Steven Curren <sfcurren249@gmail.com>
2023-07-06 11:35:05 -0500

    Add basic addition

---
+++ Calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x1;
+    int x2;
+
+    cout << "Enter the first number: ";
+    cin >> x1;
+
+    cout << "Enter the second number: ";
+    cin >> x2;
+
+    cout << "The sum is: " << (x1 + x2);
+}
